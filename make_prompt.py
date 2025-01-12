9d9ac178fa95a4375b6a6028736f21d31fad081f
emma-x1 <139976818+emma-x1@users.noreply.github.com>
2025-01-12 00:48:16 -0500

    model prompt

--- make_prompt.py
+++ make_prompt.py
@@ -1,16 +1,43 @@
+import json
 import torch
-from transformers import pipeline
+from transformers import (AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline)
 
-model_id = "meta-llama/Llama-3.2-1B-Instruct"
+config_data = json.load(open('config.json'))
+HF_TOKEN = config_data['HF_TOKEN']
+
+#config data
+model_id = "meta-llama/Llama-3.2-1B"
+
+#quantisation config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+
+#tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=HF_TOKEN)
+tokenizer.pad_token = tokenizer.eos_token
+
+#model
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, 
+    device_map="auto", 
+    quantization_config=bnb_config,
+    token=HF_TOKEN,
+    )
+
+#pipeline
 messages = [
     {"role": "user", "content": "Generate a text message prompt for a user to answer. This text message prompt should be a typical question asked in a specific type of setting. The user should be asked to provide a response to the question. If the indicated level is low, the question should be a typical, easy-to-answer question, while if the level is high, the question should require more thought, be more unexpected, and be more creative."},
 ]
 
 pipe = pipeline(
     "text-generation", 
-    model=model_id,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=256,
     )
 
 outputs = pipe(
